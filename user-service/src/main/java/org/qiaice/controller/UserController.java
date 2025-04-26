7bfd3c2de24395b263970035cc12cad738e5694f
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-04-26 16:18:42 +0800

    通过 spring.cloud.sentinel.block-page 属性配置接口层面的限流措施; 通过 @SentinelResource 的 blockHandler 属性和 fallback 属性配置方法层面的限流措施和异常处理措施

--- user-service/src/main/java/org/qiaice/controller/UserController.java
+++ user-service/src/main/java/org/qiaice/controller/UserController.java
@@ -1,5 +1,7 @@
 package org.qiaice.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.AllArgsConstructor;
 import org.qiaice.entity.User;
 import org.qiaice.service.UserService;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/user", produces = "application/json; charset=utf-8")
@@ -18,5 +23,28 @@ public class UserController {
     @GetMapping(value = "/{uid}")
     public User findByUid(@PathVariable Integer uid) {
         return userService.getById(uid);
+    }
+
+    @GetMapping(value = "/block")
+    public Map<String, Serializable> blockHandler() {
+        return Map.of(
+                "code", 400,
+                "msg", "您的访问过于频繁, 请稍后再试",
+                "data", "null"
+        );
+    }
+
+    @GetMapping(value = "/test")
+    @SentinelResource(value = "test", blockHandler = "testBlockHandler")
+    public Map<String, Serializable> test() {
+        return Map.of(
+                "code", 200,
+                "msg", "资源访问成功",
+                "data", "null"
+        );
+    }
+
+    public Map<String, Serializable> testBlockHandler(BlockException e) {
+        return blockHandler();
     }
 }
