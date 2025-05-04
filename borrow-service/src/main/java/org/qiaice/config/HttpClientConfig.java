568c2a2a935d9bb1d4d4dc0740a5b6d3e7a5280b
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-05-04 23:36:31 +0800

    在配置类 HttpClientConfig 中将请求拦截器的创建移动到客户端 RestClient 的创建中

--- borrow-service/src/main/java/org/qiaice/config/HttpClientConfig.java
+++ borrow-service/src/main/java/org/qiaice/config/HttpClientConfig.java
@@ -3,11 +3,9 @@ package org.qiaice.config;
 import org.apache.seata.core.context.RootContext;
 import org.qiaice.service.client.BookClient;
 import org.qiaice.service.client.UserClient;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -16,19 +14,13 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 public class HttpClientConfig {
 
     @Bean
-    public ClientHttpRequestInterceptor clientHttpRequestInterceptor() {
-        return (request, bytes, execution) -> {
-            request.getHeaders().add(RootContext.KEY_XID, RootContext.getXID());
-            return execution.execute(request, bytes);
-        };
-    }
-
-    @Bean
     @LoadBalanced
-    public RestClient.Builder restClientBuilder(
-            @Qualifier("clientHttpRequestInterceptor") ClientHttpRequestInterceptor interceptor
-    ) {
-        return RestClient.builder().requestInterceptor(interceptor);
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder()
+                .requestInterceptor((request, bytes, execution) -> {
+                    request.getHeaders().add(RootContext.KEY_XID, RootContext.getXID());
+                    return execution.execute(request, bytes);
+                });
     }
 
     @Bean
